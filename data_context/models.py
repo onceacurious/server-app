e3ecde18e2011191eebe93b9c9b42a79b57421b1
Francis Deo Aurelio <francisdeoaurelio.fda@gmail.com>
2023-01-10 09:22:34 +0000

    nc

--- data_context/models.py
+++ data_context/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class UserGroup(models.Model):
+  display_name = models.CharField(max_length=150, blank=True)
+  company_name = models.CharField(max_length=150, blank=True)
+
+  def __str__(self):
+    return f'{self.display_name}_{self.company_name}'
+
+
 class User(AbstractUser):
   company = models.ForeignKey(UserGroup,
                               on_delete=models.CASCADE,
@@ -11,19 +19,11 @@ class User(AbstractUser):
     return f'{self.username}-{self.company}'
 
 
-class UserGroup(models.Model):
-  display_name = models.CharField(max_length=150, blank=True)
-  company_name = models.CharField(max_length=150, blank=True)
-
-  def __str__(self):
-    return f'{self.display_name}_{self.company_name}'
-
-
 class Position(models.Model):
-  description = models.CharField(max_length=150)
-  group = models.ForeignField(PositionGroup,
-                              on_delete=models.CASCADE,
-                              related_name="position_group")
+  display_name = models.CharField(max_length=150)
+  group = models.ForeignKey('data_context.PositionGroup',
+                            on_delete=models.CASCADE,
+                            related_name="position_group")
 
   def __str__(self):
     return self.description
@@ -38,6 +38,12 @@ class PositionGroup(models.Model):
 
 class Product(models.Model):
   name = models.CharField(max_length=150)
+  pos_group = models.ForeignKey(Position,
+                                on_delete=models.CASCADE,
+                                related_name="position_group")
+  prod_group = models.ForeignKey('data_context.ProductGroup',
+                                 on_delete=models.CASCADE,
+                                 related_name="product_group")
 
   def __str__(self):
     return self.name
@@ -51,8 +57,19 @@ class ProductGroup(models.Model):
 
 
 class Que(models.Model):
-  target_group = models.ForeignKey(UserGroup, on_delete=models.CASCADE)
+  value = models.IntegerField()
+  product = models.ForeignKey(Product,
+                              on_delete=models.CASCADE,
+                              related_name="queued_product")
+  called = models.BooleanField(default=False, blank=True)
+  called_by = models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                related_name="user_caller")
+  generated_at = models.DateTimeField(auto_now=True, blank=True)
+  transaction_date = models.DateTimeField(auto_now_add=True, blank=True)
 
   @property
-  def value(self):
-    return self.target_group.count()
+  def value_name(self):
+    number = str(self.value).zfill(3)
+    char = str.lstrip(self.product)[0]
+    return f'{char}-{number}'
