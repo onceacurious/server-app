a99efb8863e947ee0f62f596a8d824804f686912
onceacurious <francisdeoaurelio.fda@gmail.com>
2023-01-15 22:54:17 +0800

    nc

--- base/consumers.py
+++ base/consumers.py
@@ -1,16 +1,27 @@
 from channels.consumer import SyncConsumer
+from asgiref.sync import async_to_sync
+import json
 
+class BaseConsumer(SyncConsumer):
+    def connect(self):
+        self.action_name = "base"
 
-class EchoConsumer(SyncConsumer):
+        async_to_sync(self.channel_layer.group_add)(
+            self.action_name
+        )
 
-    def connect(self, event):
-        self.send({
-            "type": "websocket.accept",
-            "message": "WS connected"
-        })
+    def disconnect(self, close_code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.action_name
+        )
 
-    def receive(self, event):
-        self.send({
-            "type": "websocket.send",
-            "text": event["text"],
-        })
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        data = text_data_json["data"]
+        async_to_sync(self.channel_layer.group_send)(
+            self.action_name, {"type": "base", "data": data}
+        )
+
+    def base_data(self, event):
+        data = event("data")
+        self.send(text_data=json.dumps({"data": data}))
