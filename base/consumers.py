aa2a54b09c164ace2a30282d7326598b3c5acdad
onceacurious <francisdeoaurelio.fda@gmail.com>
2023-01-16 00:40:28 +0800

    nc

--- base/consumers.py
+++ base/consumers.py
@@ -1,27 +1,30 @@
-from channels.consumer import SyncConsumer
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-class BaseConsumer(SyncConsumer):
-    def connect(self):
+class BaseConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.action_name = "base"
 
-        async_to_sync(self.channel_layer.group_add)(
-            self.action_name
+        await self.channel_layer.group_add(
+            self.action_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.action_name,
+            self.channel_name
         )
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.action_name
-        )
-
-    def receive(self, text_data):
+    async def receive(self, text_data=None):
         text_data_json = json.loads(text_data)
         data = text_data_json["data"]
-        async_to_sync(self.channel_layer.group_send)(
-            self.action_name, {"type": "base", "data": data}
+        print(data)
+        await self.channel_layer.group_send(
+            self.action_name, {"type": "base_data", "data": data}
         )
 
-    def base_data(self, event):
-        data = event("data")
-        self.send(text_data=json.dumps({"data": data}))
+    async def base_data(self, event):
+        data = event["data"]
+        await self.send(text_data=json.dumps({"type": "base", "data": data}))
--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import viewsets
 from rest_framework import status  
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render
 from rest_framework.decorators import action
 
 from data.models import *
