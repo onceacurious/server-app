5e7bc7ab9f24994e026ff45b6c2d0c6c7bbc6220
onceacurious <francisdeoaurelio.fda@gmail.com>
2023-01-20 00:49:11 +0800

    nc

--- base/serializers.py
+++ base/serializers.py
@@ -1,48 +1,60 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from data.models import *
+
 
 class UserGroupSerializer(ModelSerializer):
     class Meta:
         model = UserGroup
-        fields = '__all__'
+        fields = "__all__"
 
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = '__all__'
-        read_only_fields = ['password']
-
-class PositionGroupSerializer(ModelSerializer):
-    class Meta:
-        model = PositionGroup
-        fields = '__all__'
 
 class PositionSerializer(ModelSerializer):
     class Meta:
         model = Position
-        fields = '__all__'
+        fields = "__all__"
+
+
+class UserSerializer(ModelSerializer):
+    company = UserGroupSerializer()
+    position = PositionSerializer()
+
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "company", "position"]
+        read_only_fields = ["password", "company", "position"]
+
+
+class PositionGroupSerializer(ModelSerializer):
+    class Meta:
+        model = PositionGroup
+        fields = "__all__"
+
 
 class ProductGroupSerializer(ModelSerializer):
     class Meta:
         model = ProductGroup
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = "__all__"
+
 
 class UserLevelSerializer(ModelSerializer):
     class Meta:
         model = UserLevel
-        fields = '__all__'
+        fields = "__all__"
+
 
 class UserUpdateDateSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['password', 'firs_name', 'last_name', 'email']
+        fields = ["password", "firs_name", "last_name", "email"]
+
 
 class QueSerializer(ModelSerializer):
     class Meta:
         model = Que
-        fields = '__all__'
+        fields = "__all__"
